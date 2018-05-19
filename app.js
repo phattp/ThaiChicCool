2dd1264dcbfe91c00fea3eb3d470eb0d725f1a6f
Phatthara <phatthara.pis@gmail.com>
2018-05-19 11:20:01 +0000

    fist deploy commit

--- app.js
+++ app.js
@@ -65,5 +65,5 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 app.listen(process.env.PORT, process.env.IP, function(){
-   console.log("The YelpCamp Server Has Started!");
+   console.log("Server Has Started!");
 });
